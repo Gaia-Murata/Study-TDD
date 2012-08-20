f210700564e7245c1e33d9586888f4793fbcb25f
Mitsuo_Sugaya <Mitsuo_Sugaya@voyagegroup.com>
2012-08-21 08:22:25 +0900

    よく見たらテストが間違ってた件。PHPぇ・・・

--- study_TDD/MoneyTest.php
+++ study_TDD/MoneyTest.php
@@ -6,6 +6,6 @@ class MoneyTest extends PHPUnit_Framework_TestCase{
 	public function testMultiplication(){
 		$five = new Dollar(5);
 		$five->times(2);
-		assertEquals(10, $five->amount);
+		$this->assertEquals(10, $five->amount);
 	}
 }
