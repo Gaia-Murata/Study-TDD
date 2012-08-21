2721cff5029ecc11c6c4779342545c0a43aa573f
e-hu <e-hu@localhost.localdomain>
2012-08-21 14:37:39 +0900

    Chapter 1 updated

--- e-hu/tdd/DollarTest.php
+++ e-hu/tdd/DollarTest.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once 'Dollar.php';
-
+/**
+ * @author Ethan Hu
+ * 
+ * Chapter 1
+ */
 class DollarTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
@@ -14,9 +18,19 @@ class DollarTest extends PHPUnit_Framework_TestCase
 
     public function testMultiplication()
     {
-        $dollar = new Dollar(5);
-        $result = $dollar->times(2);
-        $this->assertEquals(10, $dollar->amount);
+        $cases = array(
+            array('amount' => 0,  'time' => 1, 'result' => 0),
+            array('amount' => 5,  'time' => 1, 'result' => 5),
+            array('amount' => 5,  'time' => 2, 'result' => 10),
+            array('amount' => 10, 'time' => 3, 'result' => 30),
+            array('amount' => 20, 'time' => 5, 'result' => 100),
+        );
+
+        foreach ($cases as $case){
+            $dollar = new Dollar($case['amount']);
+            $result = $dollar->times($case['time']);
+            $this->assertEquals($case['result'], $dollar->amount);
+        }
     }
 }
 
