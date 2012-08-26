22c2b6ddc89d57d252394147ab05392a1a6c5a49
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2012-08-27 00:53:48 +0900

    BankにMoney::dollar(3)と、Money;;dollar(4)を足しあわせてmoney::dollar(7)になるというテスとケースを作成した

--- BankTest.php
+++ BankTest.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'Bank.php';
+require_once 'Sum.php';
 
 class BankTest extends PHPUnit_Framework_TestCase
 {
@@ -10,5 +11,13 @@ class BankTest extends PHPUnit_Framework_TestCase
 		$bank = new Bank();
 		$reduced = $bank->reduce($sum, "USD");		
 		$this->assertEquals(Money::dollar(10), $reduced);
+	}
+
+	public function testReduceSum()
+	{
+		$sum = new Sum(Money::dollar(3), Money::dollar(4));		
+		$bank = new Bank();
+		$result = $bank->reduce($sum, "USD");
+		$this->assertEquals(Money::dollar(7), $result);
 	}
 }
