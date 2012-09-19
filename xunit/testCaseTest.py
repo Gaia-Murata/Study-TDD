df79b2e0dae53a5d305bb197308320bfd0a5e6ee
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2012-09-20 00:45:49 +0900

    23章完了 testSuiteのテストと実装を行った。runメソッドのインターフェースを変更した

--- xunit/testCaseTest.py
+++ xunit/testCaseTest.py
@@ -1,31 +1,45 @@
 from testCase import TestCase
 from wasRun import WasRun
 from testResult import TestResult
+from testSuite import TestSuite
 
 class TestCaseTest(TestCase):
+	def setUp(self):
+		self.result = TestResult()
+
 	def testTemplateMethod(self):
 		test= WasRun("testMethod")
-		test.run()
+		test.run(result)
 		assert("setUp testMethod tearDown " == test.log)
 	
 	def testResult(self):
 		test = WasRun("testMethod")
-		result = test.run()
+		test.run(result)
 		assert("1 run, 0 failed" == result.summary())
 	
 	def testFailedResult(self):
 		test = WasRun("testBrokenMethod")
-		result = test.run()
+		test.run(result)
 		assert("1 run, 1 failed" ==  result.summary())
 	
 	def testFailedResultFormatting(self):
-		result = TestResult()
 		result.testStarted()
 		result.testFailed()
 		assert("1 run, 1 failed" == result.summary())
+	
+	def testSuite(self):
+		suite = TestSuite()
+		suite.add(WasRun("testMethod"))
+		suite.add(WasRun("testBrokenMethod"))
+		suite.run(result)
+		assert("2 run, 1 failed" == result.summary())
 
-TestCaseTest("testTemplateMethod").run().summary()
-TestCaseTest("testResult").run().summary()
-TestCaseTest("testFailedResultFormatting").run().summary()
-TestCaseTest("testFailedResult").run().summary()
-
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testSuite"))
+result = TestResult()
+suite.run(result)
+print result.summary()
