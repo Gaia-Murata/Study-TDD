8e41d7c2dfd3cad4ea0729acf12e1f6a41ac95fc
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2012-09-19 22:42:37 +0900

    21章　runCountを変数に置き換えた。 testFailedResultとtestBrokenMethodというゴミを生み出してとりあえず放置

--- xunit/testCaseTest.py
+++ xunit/testCaseTest.py
@@ -1,10 +1,23 @@
 from testCase import TestCase
 from wasRun import WasRun
+from testResult import TestResult
 
 class TestCaseTest(TestCase):
 	def testTemplateMethod(self):
 		test= WasRun("testMethod")
 		test.run()
 		assert("setUp testMethod tearDown " == test.log)
+	
+	def testResult(self):
+		test = WasRun("testMethod")
+		result = test.run()
+		assert("1 run, 0 failed" == result.summary())
+	
+	def testFailedResult(self):
+		test = WasRun("testBrokenMethod")
+		result = test.run()
+		assert("1 run, 1 failed", result.summary)
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
