547e4dd9809a838988d96c60b105c56ba87017bf
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2012-09-19 23:00:32 +0900

    22章　testがこけた回数をカウントするテストの作成と実装を行った

--- xunit/testCaseTest.py
+++ xunit/testCaseTest.py
@@ -13,11 +13,19 @@ class TestCaseTest(TestCase):
 		result = test.run()
 		assert("1 run, 0 failed" == result.summary())
 	
-	def testFailedResult(self):
-		test = WasRun("testBrokenMethod")
-		result = test.run()
-		assert("1 run, 1 failed", result.summary)
+#	def testFailedResult(self):
+#		test = WasRun("testBrokenMethod")
+#		result = test.run()
+#		assert("1 run, 1 failed", result.summary)
+	
+	def testFailedResultFormatting(self):
+		result = TestResult()
+		result.testStarted()
+		result.testFailed()
+		assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
-TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
+#TestCaseTest("testFailedResult").run()
+
