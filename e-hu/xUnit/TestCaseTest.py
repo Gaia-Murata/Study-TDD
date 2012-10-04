d9039eeece65abb7ce1edd7a3e49cf11c2825c3c
e-hu <e-hu@localhost.localdomain>
2012-10-04 14:20:19 +0900

    24 chapter

--- e-hu/xUnit/TestCaseTest.py
+++ e-hu/xUnit/TestCaseTest.py
@@ -7,44 +7,43 @@ from TestSuite import TestSuite
 from TestResult import TestResult
 
 class TestCaseTest(TestCase):
-    def testSetUp(self):
-        self.test.run()
-	assert("setUp " == self.test.log)
+
+    def setUp(self):
+        self.result = TestResult()
     
     def testTemplateMethod(self):
         test= WasRun("testMethod")
-	test.run()
+	test.run(self.result)
 	assert("setUp testMethod tearDown " == test.log)
 
     def testResult(self):
         test = WasRun("testMethod")
-        result = test.run()
-        assert("1 run, 0 failed " == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed " == self.result.summary())
 
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = test.run()
-        assert("1 run, 0 failed " == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed " == self.result.summary())
     
     def testFailedResultFormatting(self):
-        result = TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert("1 run, 1 failed " == result.summary())
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed " == self.result.summary())
 
     def testSuite(self):
         suite = TestSuite()
         suite.add(WasRun("testMethod"))
         suite.add(WasRun("testBrokenMethod"))
-        result = TestResult()
-        suite.run(result)
-        assert("2 run, 1 failed " == result.summary())
+        suite.run(self.result)
+        assert("2 run, 1 failed " == self.result.summary())
 
 suite = TestSuite()
 suite.add(TestCaseTest("testTemplateMethod"))
 suite.add(TestCaseTest("testResult"))
 suite.add(TestCaseTest("testFailedResultFormatting"))
 suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testSuite"))
 
 result = TestResult()
 suite.run(result)
